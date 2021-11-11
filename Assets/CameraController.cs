1fada2cd632a7ee222ff507c8b1bc87c0d4e9c85
DESKTOP-F2LA4FV <pedro.vaz@twoimpulse.com>
2021-11-11 11:20:17 +0000

    Initial Commit Added Game Folder to Dev

---
+++ Assets/CameraController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraController: MonoBehaviour {
+
+    public Transform target;
+
+    void Update() {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }
+}
